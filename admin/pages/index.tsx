43503a282b9f134174b7e74f5806a7ae6874e578
Vojta Staněk <stanekv01@gmail.com>
2022-07-04 11:46:03 +0200

    add renderOption

--- admin/pages/index.tsx
+++ admin/pages/index.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react'
-import { DataGrid, DataGridPage, GenericPage, HasManySelectCell, TextCell, TextField } from '@contember/admin'
+import { DataGrid, DataGridPage, Field, FieldView, GenericPage, HasManySelectCell, TextCell, TextField } from '@contember/admin'
 
 export default () => (
 	<DataGridPage entities="Article">
 		<TextCell field="title" header="Title" />
-		<HasManySelectCell field="tags[show = true]" options="Tag[show = true].name" header="Tags" />
+		<HasManySelectCell
+			header="Tags"
+
+			field="tags[show = true]"
+			
+			options={{ entities: "Tag", filter: "[show = true]" }}
+			renderOption={(acc) => <>➡️{acc.getField('name').value}⬅️</>}
+			optionsStaticRender={<Field field="name" />}
+		/>
 	</DataGridPage>
 )
